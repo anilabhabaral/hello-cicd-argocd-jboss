71b1e200d3a70c726f4556872d917c3cb1fd98e8
Anilabha Baral <85929177+anilabhabaral@users.noreply.github.com>
2024-01-26 20:26:12 +0530

    Update HelloService.java

--- src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -25,7 +25,7 @@ package org.jboss.as.quickstarts.helloworld;
 public class HelloService {
 
     String createHelloMessage(String name) {
-        return "Hello Anilabha " + name + "!";
+        return "Hello " + name + "!";
     }
 
 }
