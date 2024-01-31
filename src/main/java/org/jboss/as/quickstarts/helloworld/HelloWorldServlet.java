8d7b113260e3f96302bf01488ed325a5344d48a1
Anilabha Baral <85929177+anilabhabaral@users.noreply.github.com>
2024-01-31 19:29:38 +0530

    Update HelloWorldServlet.java

--- src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
@@ -55,7 +55,7 @@ public class HelloWorldServlet extends HttpServlet {
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println(PAGE_HEADER);
-        writer.println("<h1>" + helloService.createHelloMessage("World") + "</h1>");
+        writer.println("<h1>" + helloService.createHelloMessage("Anilabha") + "</h1>");
         writer.println(PAGE_FOOTER);
         writer.close();
     }
